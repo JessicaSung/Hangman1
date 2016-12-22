e37e413dd73bdc480759971671df895a51b581ce
JessicaSung <jessica.sung@utexas.edu>
2016-12-22 11:46:23 -0600

    Created section to notify user how many guesses remaining (need to finish logic); changed wins and losses from div to span.

--- app.js
+++ app.js
@@ -3,20 +3,26 @@
 // chosenWord is the word the user is trying to guess
 // currentProgress shows the word as the user guess with unguessed letters as _
 // allGuesses are all the unique letters the user has guessed
+// guessesRemaining is how many guesses the user has left
 var currentProgress = [];
 var allGuesses = [];
+var guessesRemaining = 0;
 	
 // create array with possible word choices
 var possibleWords = ['coding', 'html', 'running'];
 // choose a word at random
 var chosenWord = possibleWords[Math.floor(Math.random()*possibleWords.length)];
 console.log(chosenWord);
+// tell user how many guesses he has left
+guessesRemaining = chosenWord.length + 10;
+$('#guessesLeft').append(guessesRemaining);
+console.log(guessesRemaining);
 
 // currentProgress _ equal to the number of letters in the chosen word
 for (var i = 0; i < chosenWord.length; i++) {
 	chosenWord[i] = currentProgress.push("_ ");
 }
-console.log(currentProgress);
+// console.log(currentProgress);
 // append _ word to the page
 $('#word').append(currentProgress);
 
@@ -24,7 +30,6 @@ $('#word').append(currentProgress);
 $(document).keydown(function(event) {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	allGuesses.push(userGuess);
-	console.log(allGuesses);
 	$('#wordTwo').empty();
 	$('#wordTwo').append(allGuesses);
 	if (chosenWord.indexOf(userGuess) > -1) {
