e1523d453d2277ff952ba566bfc9be621bec421e
JessicaSung <jessica.sung@utexas.edu>
2016-12-28 14:31:10 -0600

    Fixed duplicate app.js file.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,42 @@
 // VARIABLES
 // ===============================================
 // chosenWord is the word the user is trying to guess
-// display is the current progress of user guess
+// currentProgress shows the word as the user guess with unguessed letters as _
 // allGuesses are all the unique letters the user has guessed
-var display = [];
+// guessesRemaining is how many guesses the user has left
+var currentProgress = [];
 var allGuesses = [];
+var guessesRemaining = 0;
 	
 // create array with possible word choices
 var possibleWords = ['coding', 'html', 'running'];
 // choose a word at random
 var chosenWord = possibleWords[Math.floor(Math.random()*possibleWords.length)];
 console.log(chosenWord);
+// tell user how many guesses he has left
+guessesRemaining = chosenWord.length + 10;
+$('#guessesLeft').append(guessesRemaining);
+console.log(guessesRemaining);
 
-// display _ equal to the number of letters in the chosen word
+// currentProgress _ equal to the number of letters in the chosen word
 for (var i = 0; i < chosenWord.length; i++) {
-	chosenWord[i] = display.push("_ ");
+	chosenWord[i] = currentProgress.push("_ ");
 }
-console.log(display);
+// console.log(currentProgress);
 // append _ word to the page
-$('#word').append(display);
+$('#word').append(currentProgress);
 
 // log user input
 $(document).keydown(function(event) {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	allGuesses.push(userGuess);
-	console.log(allGuesses);
 	$('#wordTwo').empty();
 	$('#wordTwo').append(allGuesses);
 	if (chosenWord.indexOf(userGuess) > -1) {
 		var index = chosenWord.indexOf(userGuess);
-		display[index] = userGuess;
+		currentProgress[index] = userGuess;
 		$('#word').empty();
-		$('#word').append(display);
+		$('#word').append(currentProgress);
 	}
 });
 
