37d9e59d2c8e0166e37a15f97f627924fe4821f3
JessicaSung <jessica.sung@utexas.edu>
2016-12-28 16:57:35 -0600

    Working on resetting the game after winning.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,31 +6,37 @@
 // guessesRemaining is how many guesses the user has left
 var currentProgress = [];
 var allGuesses = [];
+var chosenWord = "";
 var guessesRemaining = 0;
 var wins = 0;
 var losses = 0;
-	
-// create array with possible word choices
-var possibleWords = ['coding', 'html', 'running'];
-// choose a word at random
-var chosenWord = possibleWords[Math.floor(Math.random()*possibleWords.length)];
-console.log(chosenWord);
-// tell user how many guesses he has left
-guessesRemaining = chosenWord.length + 10;
-$('#guessesLeft').append(guessesRemaining);
-// show user how many wins he has
-$('#wins').append(wins);
-// show user how many losses he has
-$('#losses').append(losses);
 
-// show user currentProgress (and how many letters are in the chosenWord)
-// currentProgress _ equal to the number of letters in the chosenWord
-for (var i = 0; i < chosenWord.length; i++) {
-	chosenWord[i] = currentProgress.push("_ ");
+function start() {	
+	// create array with possible word choices
+	var possibleWords = ['coding', 'html', 'running'];
+	// choose a word at random
+	chosenWord = possibleWords[Math.floor(Math.random()*possibleWords.length)];
+	console.log(chosenWord);
+	// tell user how many guesses he has left
+	guessesRemaining = chosenWord.length + 10;
+	$('#guessesLeft').append(guessesRemaining);
+	// show user how many WINS he has
+	$('#wins').empty();
+	$('#wins').append(wins);
+	// show user how many LOSSES he has
+	$('#losses').empty();
+	$('#losses').append(losses);
+
+	// Guess This Word
+	// show user currentProgress (and how many letters are in the chosenWord)
+	// currentProgress _ equal to the number of letters in the chosenWord
+	for (var i = 0; i < chosenWord.length; i++) {
+		chosenWord[i] = currentProgress.push("_ ");
+	}
+	// append _ word to the page
+	$('#currentProgress').empty();
+	$('#currentProgress').append(currentProgress);
 }
-// console.log(currentProgress);
-// append _ word to the page
-$('#currentProgress').append(currentProgress);
 
 // LOG USER INPUT
 // ===============================================
@@ -63,17 +69,30 @@ $(document).keydown(function(event) {
 
 	// User Wins
 	// if user has replaced all _ with the correct letter
+	// increment the number of wins and ask user to play again
 	if (currentProgress.indexOf("_ ") == -1) {
 		wins += 1;
 		$('#wins').empty();
 		$('#wins').append(wins);
 		setTimeout(function() { 
-			alert("Congratulations! You've won =)"); 
+			var again = confirm("Congratulations! You won.\n\nPlay again?");
+			if (again == true) {
+				var currentProgress = [];
+				var allGuesses = [];
+				var guessesRemaining = 0;
+				$('#currentProgress').empty();
+				$('#guessesLeft').empty();
+				$('#lettersGuessed').empty();
+				start();
+			}
 		}, 250);
 	}
 
 	// User Loses
 	// if user has run out of guesses
+	// if () {
+
+	// }
 });
 
     
@@ -115,5 +134,4 @@ $(document).keydown(function(event) {
 
 
 
-// $(document).ready(function() {
-// });
+start();
